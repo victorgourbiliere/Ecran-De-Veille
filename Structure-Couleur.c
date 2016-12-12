1e2f90857ad51530153b726e896594aa4c0dfa26
victorgourbiliere <victor.gourbiliere@viacesi.fr>
2016-12-12 14:09:30 +0100

    Add files via upload

---
+++ Structure-Couleur.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Couleur
+{
+    // Ces variables représentent les couleurs, R pour red, G pour green et B pour blue.
+    int R;
+    int G;
+    int B;
+
+}Couleur;
+
+int main()
+{
+}
